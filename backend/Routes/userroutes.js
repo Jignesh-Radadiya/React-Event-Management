443e07c3d872e6d299d78444054c833cd47deec9
Royalrider07 <132362850+Royalrider07@users.noreply.github.com>
2024-04-02 11:59:16 +0530

    Delete backend/Routes directory

--- backend/Routes/userroutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../Controllers/usercontroller');
-
-router.post('/register', userController.registerUser);
-
-module.exports = router;
